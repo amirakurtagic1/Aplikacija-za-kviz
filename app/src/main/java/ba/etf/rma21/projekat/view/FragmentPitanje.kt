560c8f00900ddb6bcdfa16ae5376a9848367c4a9
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-04-25 01:39:15 +0200

    dodato otvaranje fragmenta sa pitanjima u fragmentPokusaj

--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentPitanje.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentPitanje.kt
@@ -1,6 +1,47 @@
 package ba.etf.rma21.projekat.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ba.etf.rma21.projekat.R
+import ba.etf.rma21.projekat.data.models.Pitanje
 
-class FragmentPitanje: Fragment() {
+class FragmentPitanje(pitanje: Pitanje): Fragment() {
+
+    private lateinit var tekstPitanja: TextView
+    private lateinit var odgovoriLista: ListView
+    private var pitanje = pitanje
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_pitanje, container, false)
+
+        tekstPitanja = view.findViewById(R.id.tekstPitanja)
+        odgovoriLista = view.findViewById(R.id.odgovoriLista)
+
+        tekstPitanja.setText(pitanje.tekst)
+        val arrayAdapter: ArrayAdapter<*>
+        val listaPitanja = pitanje.opcije
+
+        val adapterLista = activity?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_list_item_1, listaPitanja
+            )
+        }
+        odgovoriLista.adapter = adapterLista
+
+        return view;
+    }
+
+    companion object {
+        fun newInstance(pitanje: Pitanje): FragmentPitanje = FragmentPitanje(pitanje);
+    }
 }
--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentPokusaj.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentPokusaj.kt
@@ -1,23 +1,34 @@
 package ba.etf.rma21.projekat.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import ba.etf.rma21.projekat.MainActivity
 import ba.etf.rma21.projekat.R
 import ba.etf.rma21.projekat.data.models.Pitanje
+import ba.etf.rma21.projekat.data.repositories.PitanjaKvizRepository
+import ba.etf.rma21.projekat.data.repositories.PitanjeKvizRepository
 import com.google.android.material.navigation.NavigationView
 
 class FragmentPokusaj(listaPitanja: List<Pitanje>): Fragment() {
-    protected open var bottomNavigationViewVisibility = View.VISIBLE
 
 
     private lateinit var navigacijaPitanja: NavigationView
     private lateinit var framePitanje: FrameLayout
+    lateinit var context: AppCompatActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.context = context as AppCompatActivity
+    }
+    private var listaPitanja = listaPitanja
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,6 +42,17 @@ class FragmentPokusaj(listaPitanja: List<Pitanje>): Fragment() {
         }
         navigacijaPitanja = view.findViewById(R.id.navigacijaPitanja)
         framePitanje = view.findViewById(R.id.framePitanje)
+        navigacijaPitanja.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener {
+            val fm = context.supportFragmentManager
+            val fragmentTransaction: FragmentTransaction
+            val fragment = FragmentPitanje(listaPitanja.get(it.title.toString().toInt() - 1))
+            fragmentTransaction = fm.beginTransaction()
+            fragmentTransaction.replace(R.id.framePitanje, fragment)
+                .addToBackStack(null)
+            fragmentTransaction.commit()
+             true
+        })
+
 
 
         return view;
