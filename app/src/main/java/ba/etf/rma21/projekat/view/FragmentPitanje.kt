541b86869690a1987efe9ae84da14dcb706bfc20
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-04-25 14:44:16 +0200

    dodata fun onClick gdje  se osluskuje selektovani item iz listView

--- app/src/main/java/ba/etf/rma21/projekat/view/FragmentPitanje.kt
+++ app/src/main/java/ba/etf/rma21/projekat/view/FragmentPitanje.kt
@@ -1,21 +1,31 @@
 package ba.etf.rma21.projekat.view
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import ba.etf.rma21.projekat.R
 import ba.etf.rma21.projekat.data.models.Pitanje
 
-class FragmentPitanje(pitanje: Pitanje): Fragment() {
+
+class FragmentPitanje(pitanje: Pitanje): Fragment(), AdapterView.OnItemClickListener {
 
     private lateinit var tekstPitanja: TextView
     private lateinit var odgovoriLista: ListView
     private var pitanje = pitanje
+
+    private var arrayAdapter:ArrayAdapter<String>? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,21 +37,47 @@ class FragmentPitanje(pitanje: Pitanje): Fragment() {
         odgovoriLista = view.findViewById(R.id.odgovoriLista)
 
         tekstPitanja.setText(pitanje.tekst)
-        val arrayAdapter: ArrayAdapter<*>
         val listaPitanja = pitanje.opcije
+
+    //    val listaPitanja = arrayOf(pitanje.opcije.get(0))
 
         val adapterLista = activity?.let {
             ArrayAdapter(
                 it,
-                android.R.layout.simple_list_item_1, listaPitanja
+                R.layout.odgovor, listaPitanja
             )
         }
         odgovoriLista.adapter = adapterLista
+        odgovoriLista.choiceMode = ListView.CHOICE_MODE_SINGLE
+        odgovoriLista.onItemClickListener = this
+
+        adapterLista?.setDropDownViewResource(R.layout.odgovor)
+
+  //  arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, listaPitanja)
+
 
         return view;
     }
 
     companion object {
         fun newInstance(pitanje: Pitanje): FragmentPitanje = FragmentPitanje(pitanje);
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    @SuppressLint("ResourceAsColor")
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if (parent != null) {
+            //println(parent.getItemAtPosition(position).toString() + " a pravi odgovor je: " + pitanje.tacan)
+            if(position.equals(pitanje.tacan)) {
+                parent.getChildAt(position).setBackgroundResource(R.color.correctAnswer)
+                odgovoriLista.isEnabled = false
+            }
+            else{
+                parent.getChildAt(position).setBackgroundResource(R.color.wrongAnswer)
+                parent.getChildAt(pitanje.tacan).setBackgroundResource(R.color.correctAnswer)
+                odgovoriLista.isEnabled = false;
+            }
+        }
     }
 }
