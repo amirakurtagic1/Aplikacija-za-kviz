24ea30ac6bf48176f3d601a03e893cbd574cbaf6
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-05-29 23:31:39 +0200

    dodati testovi i hash kod za trecu spiralu

---
+++ app/src/main/java/ba/etf/rma21/projekat/data/repositories/AccountRepository.kt
@@ -0,0 +1,21 @@
+package ba.etf.rma21.projekat.data.repositories
+
+
+class AccountRepository {
+
+    companion object {
+        //TODO Ovdje trebate dodati hash string vašeg accounta
+        var acHash: String = "http://rma21-etf.herokuapp.com/account/e9336960-63fd-4158-af6a-a7a2b2984288"
+
+        fun postaviHash(acHash: String): Boolean {
+            this.acHash = acHash
+            return true
+        }
+
+        fun getHash(): String {
+            return acHash
+        }
+
+
+    }
+}
